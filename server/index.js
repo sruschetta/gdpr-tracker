82f5886e52ba4d73379a8d451183c1bd82bfbda4
stefanoruschettacaleffi <stefano.ruschetta@caleffi.com>
2019-04-12 11:47:46 +0200

    2012-2011

--- server/index.js
+++ server/index.js
@@ -49,7 +49,7 @@ fs.readdir(dir, function(err, items) {
 
 var cron = require('node-cron');
 
-cron.schedule('45 08 * * 1-5', () => {
+cron.schedule('00 10 * * 1-5', () => {
   importer.sendOldDocuments(100);
 });
 
